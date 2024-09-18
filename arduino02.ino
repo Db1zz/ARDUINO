8d11303c027ff337684f48f868070e1ca139f05c
tamasandor <111676834+tamasandor@users.noreply.github.com>
2024-09-18 21:39:02 +0200

    Finally

--- arduino02.ino
+++ arduino02.ino
@@ -7,6 +7,7 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 int current_potentio_meter = 0;
 int    brightness = 0;
 int com_input = 0;
+bool  useSerialInput = false;
 
 void clearSerialBuffer()
 {
@@ -27,30 +28,33 @@ void setup() {
 
 void loop() {
  int    sensor_val = analogRead(A0);
- if (current_potentio_meter != sensor_val)
- {
-  current_potentio_meter = sensor_val;
-  brightness = current_potentio_meter/4;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Hello Comrades");
-  lcd.setCursor(0, 1);
-  lcd.print(String("brightness: ") + String(brightness));
- }
- else if (Serial.available() > 0)
- {
-  com_input = Serial.parseInt();
-  brightness = com_input;
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print("Hello Comrades");
-  lcd.setCursor(0, 1);
-  lcd.print(String("brightness: ") + String(brightness));
- }
- clearSerialBuffer();
+ if (!useSerialInput && current_potentio_meter != sensor_val) {
+    current_potentio_meter = sensor_val;
+    brightness = current_potentio_meter / 4;
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Hello Comrades");
+    lcd.setCursor(0, 1);
+    lcd.print("brightness: " + String(brightness));
+  }
 
- 
-
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    if (input == "SWITCH") {
+      useSerialInput = false;
+    } else {
+      com_input = input.toInt();
+      brightness = com_input % 256;
+      useSerialInput = true;
+    }
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Hello Comrades");
+    lcd.setCursor(0, 1);
+    lcd.print("brightness: " + String(brightness));
+    clearSerialBuffer();
+  }
+  
  float  voltage = sensor_val * (5.0 / 1023.0);
 
 
