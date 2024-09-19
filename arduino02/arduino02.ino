10c334bdc402c4201f50d18f19b20d75befb3130
Georgijs Onischenko <xel27@inbox.lv>
2024-09-19 14:02:18 +0200

    fix output:

--- arduino02/arduino02.ino
+++ arduino02/arduino02.ino
@@ -1,26 +1,25 @@
 #include <LiquidCrystal.h>
 
 const int rs = 10, en = 9, d4 = 8, d5 = 7, d6 = 6, d7 = 5;
-
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
-int   old_sensor_val = 0;
+float  voltage = 0;
+int   new_p_meter_val = 0;
+int   old_p_meter_val = 0;
 int   brightness = 0;
 int   com_input = 0;
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void clear_serial_buffer()
 {
   while (Serial.available() > 0)
-  {
     Serial.read();
-  }
 }
 
-void  update_display(int brightness)
+void  update_display(int brightness, int p_meter_val)
 {
     lcd.clear();
     lcd.setCursor(0, 0);
-    lcd.print("Hello Comrades");
+    lcd.print("p_meter_val:" + String(p_meter_val));
     lcd.setCursor(0, 1);
     lcd.print("brightness: " + String(brightness));
 }
@@ -39,26 +38,20 @@ void setup() {
 }
 
 void loop() {
- int    new_sensor_val = analogRead(A0) / 4;
- if (is_new_signal_changed(new_sensor_val, old_sensor_val)) {
-    old_sensor_val = new_sensor_val;
-    brightness = old_sensor_val;
-    update_display(brightness);
+ new_p_meter_val = analogRead(A0);
+ if (is_new_signal_changed(new_p_meter_val, old_p_meter_val)) {
+    old_p_meter_val = new_p_meter_val;
+    brightness = old_p_meter_val / 4;
+    update_display(brightness, old_p_meter_val);
   }
-
   if (Serial.available() > 0) {
     com_input = Serial.parseInt();
     brightness = com_input % 256;
-    update_display(brightness);
+    update_display(brightness, old_p_meter_val);
     clear_serial_buffer();
   }
-  
- float  voltage = new_sensor_val * (5.0 / 1023.0);
-
-
- analogWrite(11, brightness);
- Serial.println(brightness);
- delay(200);
-
-
+  voltage = new_p_meter_val * (5.0 / 1023.0);
+  analogWrite(11, brightness);
+  Serial.println(brightness);
+  delay(200);
 }
